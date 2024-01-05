be29a198d474cd2105f7266e4f13ca084d559fa1
Christian <ferchris82@hotmail.com>
2024-01-05 14:04:40 -0500

    Versión 1.0

---
+++ src/org/chrisferdev/ejemplos/set/EjemploLinkedList.java
@@ -0,0 +1,51 @@
+package org.chrisferdev.ejemplos.set;
+
+import org.chrisferdev.ejemplos.modelo.Alumno;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EjemploLinkedList {
+    public static void main(String[] args) {
+
+        LinkedList<Alumno> enlazada = new LinkedList<>();
+        System.out.println(enlazada + ", size = " + enlazada.size());
+        System.out.println("esta vacía = " + enlazada.isEmpty());
+        enlazada.add(new Alumno("Pato", 5));
+        enlazada.add(new Alumno("Cata", 6));
+        enlazada.add(new Alumno("Luci", 4));
+        enlazada.add(new Alumno("Jano", 7));
+        enlazada.add(new Alumno("Andres", 3));
+
+        System.out.println(enlazada + ", size = " + enlazada.size());
+
+        enlazada.addFirst(new Alumno("Zeus", 5));
+        enlazada.addLast(new Alumno("Atenea", 6));
+        System.out.println(enlazada + " size = " + enlazada.size());
+
+        System.out.println("Primero = " + enlazada.getFirst());
+        System.out.println("Último = " + enlazada.getLast());
+        System.out.println("Indice 2 = " + enlazada.get(2));
+
+        //Alumno zeus = enlazada.pop();
+        //enlazada.pollLast();
+        enlazada.removeFirst();
+        enlazada.removeLast();
+        System.out.println(enlazada + " size = " + enlazada.size());
+
+        enlazada.remove(new Alumno("Jano", 7));
+        System.out.println(enlazada + " size = " + enlazada.size());
+
+        Alumno a = new Alumno("Lucas", 5);
+        enlazada.addLast(a);
+        System.out.println("Indice de Lucas = " + enlazada.indexOf(a));
+
+        enlazada.remove(2);
+        System.out.println(enlazada + " size = " + enlazada.size());
+        System.out.println("Indice de Lucas = " + enlazada.indexOf(a));
+
+        enlazada.set(3, new Alumno("Lalo", 7));
+        System.out.println(enlazada + " size = " + enlazada.size());
+    }
+}
